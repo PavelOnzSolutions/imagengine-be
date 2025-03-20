d79769703a8942c76d6a10f8dc0361f8b1d309bb
Pavel Onz <pavel@onz.solutions>
2025-03-20 17:20:30 +0100

    redis cfg

--- src/main/java/solutions/onz/services/imagengine/web/rest/AccountController.java
+++ src/main/java/solutions/onz/services/imagengine/web/rest/AccountController.java
@@ -1,4 +1,14 @@
 package solutions.onz.services.imagengine.web.rest;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * REST controller for managing the current user's account.
+ */
+@Slf4j
+@RestController
+@RequestMapping("/api")
 public class AccountController {
 }

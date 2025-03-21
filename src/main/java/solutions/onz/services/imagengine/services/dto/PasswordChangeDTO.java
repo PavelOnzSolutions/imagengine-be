ebe393249e320d8d919a8652c664485cc5f668c3
Pavel Onz <pavel@onz.solutions>
2025-03-21 11:50:56 +0100

    mongock

--- src/main/java/solutions/onz/services/imagengine/services/dto/PasswordChangeDTO.java
+++ src/main/java/solutions/onz/services/imagengine/services/dto/PasswordChangeDTO.java
@@ -14,7 +14,7 @@ import java.io.Serializable;
 public class PasswordChangeDTO implements Serializable {
 
     @Serial
-    private static final long serialVeSrsionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
     private String currentPassword;
     private String newPassword;
